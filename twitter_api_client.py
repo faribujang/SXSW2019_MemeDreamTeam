11d573fee81319bc1c05ef2cc2ac5f1a7d22eb43
faribujang <faribujang@gmail.com>
2019-03-13 00:40:22 -0500

    Update twitter_api_client.py

--- twitter_api_client.py
+++ twitter_api_client.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from tweepy import API
+from tweepy import API #tweepy is the python-twitter api helper thing
 from tweepy import OAuthHandler
 
 # TWITTER_CONSUMER_KEY = "j0zhTcAe9pM4wwtSCJwaVCh89"
@@ -8,7 +8,7 @@ from tweepy import OAuthHandler
 # TWITTER_ACCESS_TOKEN = "825406380161036289-Y1wsMZ1kYzRRrg2SDqUkMe1coDU0NTN"
 # TWITTER_ACCESS_SECRET = "mWChjTWuUwnsO7VsOVxG9XvDrRgDOGReasxj7fY94w9H4"
 
-def get_twitter_auth():
+def get_twitter_auth(): #initializing all the stuff
   try:
     # consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
     # consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
@@ -19,14 +19,14 @@ def get_twitter_auth():
     access_token = "825406380161036289-Y1wsMZ1kYzRRrg2SDqUkMe1coDU0NTN"
     access_secret = "mWChjTWuUwnsO7VsOVxG9XvDrRgDOGReasxj7fY94w9H4"
 
-  except KeyError:
+  except KeyError: #if the keys dont return an acceptable value, return error code
     sys.stderr.write("TWITTER_* environment variables not set \n")
     sys.exit(1)
-  auth = OAuthHandler(consumer_key, consumer_secret)
+  auth = OAuthHandler(consumer_key, consumer_secret) #authorization functions from tweepy
   auth.set_access_token(access_token, access_secret)
   return auth
 
-def get_twitter_client():
+def get_twitter_client(): #running all the stuff
   auth = get_twitter_auth()
   client = API(auth)
   return client
