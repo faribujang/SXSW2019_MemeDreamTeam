8c6a09885354ec87398f69a4064be7ec4263aeed
faribujang <faribujang@gmail.com>
2019-03-13 00:46:38 -0500

    Update twitter_get_trending.py

--- twitter_get_trending.py
+++ twitter_get_trending.py
@@ -6,8 +6,8 @@ access_token_secret = "INSERT YOUR OWN"
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth) #TBH IDK HOW THIS STUFF WORKS, IT'S QUERYING FROM THE API MODULE IN TWEEPY PACKAGE
 # trends1 = api.trends_place(1)
-trends2 = api.trends_closest(30.267153,-97.7430608) #TBH IDK HOW THIS STUFF WORKS, IT'S QUERYING FROM THE API MODULE IN TWEEPY PACKAGE
+trends2 = api.trends_closest(30.267153,-97.7430608) #getting stuff from Austin location
 
 print(trends1)
