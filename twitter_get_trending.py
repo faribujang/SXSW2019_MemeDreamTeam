b9786f78da17da40489c2416282b619989b0ec0a
faribujang <faribujang@gmail.com>
2019-03-13 00:45:24 -0500

    Update twitter_get_trending.py

--- twitter_get_trending.py
+++ twitter_get_trending.py
@@ -1,8 +1,8 @@
 import tweepy
-consumer_key = "j0zhTcAe9pM4wwtSCJwaVCh89"
-consumer_secret = "ckYkM1dV4CyOhIY408k2keJgJ3BfsNmuwQbMGmzWMk0VzBI0Kk"
-access_token = "825406380161036289-Y1wsMZ1kYzRRrg2SDqUkMe1coDU0NTN"
-access_token_secret = "mWChjTWuUwnsO7VsOVxG9XvDrRgDOGReasxj7fY94w9H4"
+consumer_key = "INSERT YOUR OWN"
+consumer_secret = "INSERT YOUR OWN"
+access_token = "INSERT YOUR OWN"
+access_token_secret = "INSERT YOUR OWN"
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
