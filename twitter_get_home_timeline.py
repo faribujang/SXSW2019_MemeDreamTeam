8a0a4e8cffd7583c8fbe35e6fa96dee375bd3e45
faribujang <faribujang@gmail.com>
2019-03-13 00:43:14 -0500

    Update twitter_get_home_timeline.py

--- twitter_get_home_timeline.py
+++ twitter_get_home_timeline.py
@@ -2,13 +2,13 @@ import json
 from tweepy import Cursor
 from twitter_api_client import get_twitter_client
 
-if __name__ == "__main__":
-  client = get_twitter_client()
+if __name__ == "__main__": #basically just runs all the functions
+  client = get_twitter_client() #getting info from previous file
 
-  with open("home_timeline.json1", "w") as f:
-    for page in Cursor(client.home_timeline, count = 200).pages(4):
+  with open("home_timeline.json1", "w") as f: #opening file, writing to it
+    for page in Cursor(client.home_timeline, count = 200).pages(4): #returning past 200 tweets from feed(?)
       for status in page:
-        f.write(json.dumps(status._json) + "\n")
+        f.write(json.dumps(status._json) + "\n") #writing it all to a returned json file
 
 
 
